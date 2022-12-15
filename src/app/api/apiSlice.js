059b7f9feb96561110ad7bcf4a0aeed7937c12cb
Gulam Gaush <mohammadgulamgaush@gmail.com>
2022-12-15 11:59:01 +0530

    implemented redux state management for fetching data

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000'
+        baseUrl: 'http://localhost:3500'
     }),
     tagTypes: ['Note', 'User'],
     endpoints: bulider => ({})
